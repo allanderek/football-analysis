2ee08937e612881a93266fc57ca0760bd3b15c51
allanderek <allan.clark@gmail.com>
2015-08-26 17:42:14 +0100

    Added a script to check all the notebook posts are still runnable. This was a problem that they all reference 'league_analysis' but I'm continually updating that and hence may be breaking some older posts. That is not a huge problem their outputs have been since computed and hence will still be under source code control, but still it would be nice to be able to just re-run all posts. For example I may improve the way that matches are displayed and it would be nice to retroactively apply that to older posts.

---
+++ blog/posts/check_all_notebooks.py
@@ -0,0 +1,22 @@
+import os
+
+def check_notebook(notebook):
+    return os.system('runipy {0} --html test.html'.format(notebook))
+    
+
+def run():
+    all_files = os.listdir()
+    notebooks = [f for f in all_files if f.endswith('.ipynb')]
+    nb_returns = [(nb, check_notebook(nb)) for nb in notebooks]
+    all_good = [nb for nb, r in nb_returns if r == 0]
+    all_bad = [nb for nb, r in nb_returns if r != 0]
+    print("Following notebooks did not return errors:")
+    for notebook in all_good:
+        print("    {0}".format(notebook))
+    print("--------------------------------------")
+    print("Following notebooks returned errors:")
+    for notebook in all_bad:
+        print("    {0}".format(notebook))
+
+if __name__ == '__main__':
+    run()
