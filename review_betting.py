d81e1c0ebd2ac9274af375406edcae70963e637d
allanderek <allan.clark@gmail.com>
2015-12-03 11:49:00 +0000

    Small updates to the review of betting to see what the post-match odds are estimated as, and try to calculate whether or not we got a decent deal.

--- review_betting.py
+++ review_betting.py
@@ -1,6 +1,21 @@
 import blog.posts.league_analysis as league_analysis
 import collections
 
+def result_count_factory():
+    return {'H': 0, 'D': 0, 'A': 0}
+    
+def post_match_odds(matches):
+    sotr_buckets = collections.defaultdict(result_count_factory)
+    tsr_buckets = collections.defaultdict(result_count_factory)
+    for match in matches:
+        sotr_diff = match.HST - match.AST
+        sotr_buckets[sotr_diff][match.FTR] += 1
+        tsr_diff = match.HS - match.AS
+        tsr_buckets[tsr_diff][match.FTR] += 1
+    return sotr_buckets, tsr_buckets
+
+sotr_buckets, tsr_buckets = post_match_odds(league_analysis.get_all_matches())
+
 def display_table(headers, rows):
     cell_size = 16
     def pad_cell(s):
@@ -112,6 +127,38 @@ class BetLine(object):
         else:
             return -1.0
 
+    def calculate_post_match_odds(self, shots_ratio):
+        match = self.match
+        if shots_ratio == "SOTR":
+            buckets = sotr_buckets
+            difference = match.HST - match.AST
+        else:
+            assert shots_ratio == "TSR"
+            buckets = tsr_buckets
+            difference = match.HS - match.AS
+        try:
+            bucket = buckets[difference]
+        except KeyError:
+            return None
+        total_matches = sum(bucket.values())
+        number = bucket[self.bet_result]
+        if not number:
+            return None
+        proportion = float(number) / float(total_matches)
+        implied_odds = 1.0 / proportion
+        return implied_odds
+
+    def post_match_expected_profit_loss(self, shots_ratio, commission=0.05):
+        pm_odds = self.calculate_post_match_odds(shots_ratio)
+        if pm_odds is None:
+            # Bit of a clutch but this means the odds were essentially
+            # off the charts which probably means we lost by a long way,
+            # and that the bet was essentially throwing money away.
+            return -1.0
+        expected_pay_out = (1.0 / pm_odds) * float(self.bet_price)
+        expected_profit_loss = (expected_pay_out * (1.0 - commission)) - 1.0
+        return expected_profit_loss
+
     def display_bet_analysis(self):
         match = self.match
         # So this is the number of goals we would have expected the team to have scored based on shots
@@ -129,6 +176,9 @@ class BetLine(object):
 
         profit_loss = self.get_profit_loss()
 
+        sotr_post_match_implied_odds = self.calculate_post_match_odds('SOTR')
+        tsr_post_match_implied_odds = self.calculate_post_match_odds('TSR')
+
         headers = ['', match.HomeTeam, match.AwayTeam]
         rows = [['Goals', match.FTHG, match.FTAG],
                 ['Shots', match.HS, match.AS],
@@ -139,13 +189,18 @@ class BetLine(object):
                 ['Score-Sots Diff', home_sots_score_diff, away_sots_score_diff],
                 ['Bet', self.bet_result, self.bet_result],
                 ['Odds', self.bet_price, self.bet_price],
-                ['Profit/Loss', profit_loss, profit_loss]
+                ['Profit/Loss', profit_loss, profit_loss],
+                ['Implied Sotr', sotr_post_match_implied_odds, sotr_post_match_implied_odds],
+                ['Implied TSR', tsr_post_match_implied_odds, tsr_post_match_implied_odds],
                 ]
         display_table(headers, rows)
 
 
 def profit_loss_bet_lines(bet_lines):
     return sum(bl.get_profit_loss() for bl in bet_lines)
+
+def post_match_expected_profit_loss_bet_lines(bet_lines):
+    return sum(bl.post_match_expected_profit_loss(bl) for bl in  bet_lines)
 
 def display_bet_lines_analysis(bet_lines):
     for bet_line in bet_lines:
@@ -181,30 +236,50 @@ def display_analysis_bets_file(bets_filename):
     bet_lines = read_bets_file(bets_filename)
     display_bet_lines_analysis(bet_lines)
 
+class SummaryTotals(object):
+    def __init__(self):
+        self.profit_loss = 0.0
+        self.sotr_expected_profit_loss = 0.0
+        self.tsr_expected_profit_loss = 0.0
 
 def analyse_multiple_bet_files(bet_filenames):
     bet_lines = []
     for bet_filename in bet_filenames:
         bet_lines.extend(read_bets_file(bet_filename))
 
-    leagues_profit_loss = collections.defaultdict(float)
+    leagues_profit_loss = collections.defaultdict(SummaryTotals)
     for bet_line in bet_lines:
+        league_summary = leagues_profit_loss[bet_line.league.title]
         profit_loss = bet_line.get_profit_loss()
-        leagues_profit_loss[bet_line.league.title] += profit_loss
+        league_summary.profit_loss += profit_loss
+        sotr_exp_pl = bet_line.post_match_expected_profit_loss('SOTR')
+        league_summary.sotr_expected_profit_loss += sotr_exp_pl
+        tsr_exp_pl = bet_line.post_match_expected_profit_loss('TSR')
+        league_summary.tsr_expected_profit_loss += tsr_exp_pl
 
     print('-------------------------')
     print('Totals for all bet files analysed:')
-    for league_name, profit_loss in leagues_profit_loss.items():
-        print('{0}: {1}'.format(league_name, profit_loss))
-    total_profit_loss = sum(leagues_profit_loss.values())
-    print('Overall total profit/loss: {0}'.format(total_profit_loss))
+    for league_name, summary_totals in leagues_profit_loss.items():
+        print('{0}: {1}, {2}, {3}'.format(league_name,
+                                          summary_totals.profit_loss,
+                                          summary_totals.sotr_expected_profit_loss,
+                                          summary_totals.tsr_expected_profit_loss))
+    total_profit_loss = sum(st.profit_loss for st in leagues_profit_loss.values())
+    total_exp_sotr = sum(st.sotr_expected_profit_loss for st in leagues_profit_loss.values())
+    total_exp_tsr = sum(st.tsr_expected_profit_loss for st in leagues_profit_loss.values())
+    print('Overall total profit/loss: {0}, {1}, {2}'.format(
+            total_profit_loss, total_exp_sotr, total_exp_tsr))
 
     print('----- Dividing by how you have betted ----------')
     for result in ['H', 'A', 'D']:
         relevant_bets = [bl for bl in bet_lines
                          if bl.bet_result == result]
         profit_loss = sum(bl.get_profit_loss() for bl in relevant_bets)
+        sotr_exp = sum(bl.post_match_expected_profit_loss('SOTR') for bl in relevant_bets)
+        tsr_exp = sum(bl.post_match_expected_profit_loss('TSR') for bl in relevant_bets)
         print('{0} bets total profit/loss: {1}'.format(result, profit_loss))
+        print('    bets total sotr expected profit/loss: {0}'.format(sotr_exp))
+        print('    bets total tsr expected profit/loss: {0}'.format(tsr_exp))
         print('    based on {0} bets'.format(len(relevant_bets)))
 
     print('----- Dividing by how the match ended ----------')
@@ -212,7 +287,11 @@ def analyse_multiple_bet_files(bet_filenames):
         relevant_bets = [bl for bl in bet_lines
                          if bl.match.FTR == result]
         profit_loss = sum(bl.get_profit_loss() for bl in relevant_bets)
+        sotr_exp = sum(bl.post_match_expected_profit_loss('SOTR') for bl in relevant_bets)
+        tsr_exp = sum(bl.post_match_expected_profit_loss('TSR') for bl in relevant_bets)
         print('{0} results total profit/loss: {1}'.format(result, profit_loss))
+        print('    bets total sotr expected profit/loss: {0}'.format(sotr_exp))
+        print('    bets total tsr expected profit/loss: {0}'.format(tsr_exp))
         print('    based on {0} bets'.format(len(relevant_bets)))
     
 
